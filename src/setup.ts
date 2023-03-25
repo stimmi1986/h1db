51ccc4081a4ccda7c171a45fa1efa35adac0e4bc
Stimmi86 <stimmi86@gmail.com>
2023-03-25 20:03:23 +0000

    setup breitt fyiri unit test

--- src/setup.ts
+++ src/setup.ts
@@ -1,39 +1,42 @@
 import { readFile } from 'fs/promises';
 import { createSchema, dropSchema, end, query } from './lib/db.js';
-// import dotenv from 'dotenv';
 
-// dotenv.config();
-
-async function create() {
-  const drop = await dropSchema();
-
-  if (drop) {
+async function dropDatabase() {
+  const isDropped = await dropSchema();
+  if (isDropped) {
     console.info('schema dropped');
   } else {
     console.info('schema not dropped, exiting');
     process.exit(-1);
   }
+}
 
-  const result = await createSchema();
-
-  if (result) {
+async function createDatabase() {
+  const isCreated = await createSchema();
+  if (isCreated) {
     console.info('schema created');
   } else {
     console.info('schema not created');
   }
+}
 
+async function insertData() {
   const data = await readFile('./sql/insert.sql');
-  const insert = await query(data.toString('utf-8'), []);
-
-  if (insert) {
+  const isInserted = await query(data.toString('utf-8'), []);
+  if (isInserted) {
     console.info('data inserted');
   } else {
     console.info('data not inserted');
   }
+}
 
+async function setupDatabase() {
+  await dropDatabase();
+  await createDatabase();
+  await insertData();
   await end();
 }
 
-create().catch((err) => {
+setupDatabase().catch((err) => {
   console.error('Error creating running setup', err);
 });
