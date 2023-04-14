e31a221a0db35fe3360663ea5871dc959be3f0fb
Stimmi86 <stimmi86@gmail.com>
2023-04-14 12:41:29 +0000

    CORS ERROR

--- src/lib/assertContentTypeForPostAndPatch.js
+++ src/lib/assertContentTypeForPostAndPatch.js
@@ -1,9 +1,9 @@
 export function assertContentTypeForPostAndPatch(req, res, next) {
-    if (req.method === 'POST' || req.method === 'PATCH') {
+    if (req.method === 'POST' || req.method === 'PATCH' || req.method === 'GET' || req.method === 'DELETE') {
       if (
-        req.headers['content-type'] &&
-        req.headers['content-type'] !== 'application/json' &&
-        !req.headers['content-type'].startsWith('multipart/form-data;')
+        req.headers['Content-Type'] &&
+        req.headers['Content-Type'] !== 'application/json' &&
+        !req.headers['Content-Type'].startsWith('multipart/form-data;')
       ) {
         return res.status(400).json({ error: 'body must be json or form-data' });
       }
