14e97b0b28bebbd47efddd073d2d1b1d0c0ad1c8
Stimmi86 <stimmi86@gmail.com>
2023-04-14 14:05:09 +0000

    CORS ERROR

--- src/app.ts
+++ src/app.ts
@@ -22,11 +22,11 @@ const app = express();
 app.use(express.json());
 
 
+app.use(cors);
+app.use(assertContentTypeForPostAndPatch);
 
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(router);
-app.use(cors);
-app.use(assertContentTypeForPostAndPatch);
 
 const port = 4000;
 
